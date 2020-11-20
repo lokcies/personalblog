a78980f058f7840465c5d47dbc54b436ac119897
Lokman Hakim <lokcieslok@gmail.com>
2020-11-21 00:33:00 +0800

    Renamed HomeController to DashboardController

--- routes/web.php
+++ routes/web.php
@@ -25,4 +25,4 @@ Route::resource('posts', PostsController::class);
 
 // Authentication
 Auth::routes();
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/dashboard', [App\Http\Controllers\DashboardController::class, 'index']);
