a817f1477e35026e0b0fa265fd59254420246ee1
Lokman Hakim <lokcieslok@gmail.com>
2020-11-21 00:58:28 +0800

    Added dashboard link as a dropdown in navbar

--- resources/views/inc/navbar.blade.php
+++ resources/views/inc/navbar.blade.php
@@ -40,7 +40,8 @@
                           {{ Auth::user()->name }}
                       </a>
 
-                      <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                      <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">    
+                          <a href="/dashboard" class="dropdown-item">Dashboard</a>                
                           <a class="dropdown-item" href="{{ route('logout') }}"
                              onclick="event.preventDefault();
                                            document.getElementById('logout-form').submit();">
