abe08c36790b8a8acd513569558f7f1d6fd509a8
Lokman Hakim <lokcieslok@gmail.com>
2020-11-21 00:42:24 +0800

    Renamed HomeController to Dashbboard Controller and Add Create Post Button to Dashboard

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,27 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">{{ __('Dashboard') }}</div>
+
+                <div class="card-body">
+                    @if (session('status'))
+                        <div class="alert alert-success" role="alert">
+                            {{ session('status') }}
+                        </div>
+                    @endif
+
+                    {{ __('You are logged in!') }}
+                    
+                    <br><hr>
+                    <a href="/posts/create" class="btn btn-primary">Create Post</a> <hr>
+                    <h3>Your Blog Posts</h3>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
